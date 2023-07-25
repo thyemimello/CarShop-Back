0dc8bba4df27e5422679658ceac31869c669e232
thyemimello <thyemi_mello@hotmail.com>
2023-07-25 19:12:47 -0300

    ajustes

---
+++ users/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    username = models.CharField(max_length=60, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=150)
+    cpf = models.CharField(max_length=11, unique=True)
+    brithdate = models.DateField()
+    profile_img = models.CharField(max_length=127)
+    is_advertiser = models.BooleanField(default=False)   
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True) 
+
+   
+
+
+
+    
---
+++ users/serializers.py
@@ -0,0 +1,75 @@
+from django.core.validators import MinLengthValidator
+from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+from address.models import Address
+from address.serializers import AddressSerializer
+
+from .models import User
+
+class UserSerializer(serializers.ModelSerializer):
+ 
+    class Meta:
+        depth = 1
+        model = User 
+        fields = [
+            "id",
+            "username",
+            "email",
+            "password",
+            "cpf",
+            "brithdate",
+           "profile_img",
+           "is_advertiser",       
+           "created_at",
+           "updated_at",
+           "address"
+        ]
+        read_only_fields = ["id", 
+                            "is_advertiser", 
+                            "created_at",
+                            "updated_at"
+                            ]
+        extra_kwargs = {
+            "password": {"write_only": True,
+                         "validators":[MinLengthValidator(8)]},
+            "username": {
+                "validators": [
+                    UniqueValidator(
+                        queryset=User.objects.all(),
+                        message="A user with that username already exists.",
+                    ),
+                ],
+            },
+             "email": {
+                "validators": [
+                    UniqueValidator(
+                        queryset=User.objects.all(),
+                        message="This field must be unique.",
+                    ),
+                ],
+            },
+        }
+
+
+    def create(self, validated_data: dict) -> User:
+       address = validated_data.pop("address")
+       user = User.objects.create_user(**validated_data)
+       Address.objects.create(**address, user=user)
+       return user
+    
+
+
+    def update(self, instance: User, validated_data: dict) -> User:
+        for key, value in validated_data.items():
+            if key == "password":
+                instance.set_password(value)
+            print(validated_data)
+            if key == "address":              
+                for Key_address, value_address in value.items():
+                  setattr(instance.address, Key_address, value_address)
+                  instance.save()
+            else:
+                setattr(instance, key, value)
+        instance.save()
+        return instance
+    
